8b263ad67f1deb90b257e20d2be814c75661fb4a
laizixian <zixianlai@gmail.com>
2019-09-12 14:55:32 -0400

    added comment for function get_search_result

--- Crawler/src/GoogleSearch.py
+++ Crawler/src/GoogleSearch.py
@@ -2,6 +2,12 @@ from googlesearch import search
 
 
 def get_search_result(query, result_num):
+    """
+    return the top results base on query and result_num
+    :param query: String
+    :param result_num: Int
+    :return: List of String
+    """
     url_list = search(query, tld='com', lang='en', num=result_num, stop=result_num, pause=1)
     return url_list
 
