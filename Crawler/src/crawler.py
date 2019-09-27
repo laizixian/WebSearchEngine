02152e8691087cf9a30f1a56f7cc9bf99e3cc906
laizixian <zixianlai@gmail.com>
2019-09-26 21:58:17 -0400

    added code to output error message on console

--- Crawler/src/crawler.py
+++ Crawler/src/crawler.py
@@ -34,8 +34,12 @@ class MultiThreadCrawler:
     logger.setLevel(logging.DEBUG)
     fh = logging.FileHandler("crawler.log")
     fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
     fh.setFormatter(formatter)
+    logger.addHandler(ch)
     logger.addHandler(fh)
 
     def __init__(self, keywords, worker_num):
