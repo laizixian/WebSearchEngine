2f87e1927feb42842ede0c79463849e4998e6b2a
laizixian <zixianlai@gmail.com>
2019-10-11 14:30:40 -0400

    started createInveredIndex

--- searchengine/src/main/java/edu/nyu/cs6913/createInvertedIndex.java
+++ searchengine/src/main/java/edu/nyu/cs6913/createInvertedIndex.java
@@ -1,4 +1,31 @@
 package edu.nyu.cs6913;
 
+import org.apache.commons.io.FilenameUtils;
+import org.jwat.warc.WarcReader;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class createInvertedIndex {
+    public static void main(String[] args) {
+        String inputPath = args[0];
+        String home = System.getProperty("user.home");
+        String trueInputPath = FilenameUtils.normalize(home + inputPath);
+        File inputDir = new File(trueInputPath);
+        FilenameFilter gzFilter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".gz");
+            }
+        };
+        File[] inputFiles = inputDir.listFiles(gzFilter);
+        Parser parser = new Parser();
+        assert inputFiles != null;
+        for (File f : inputFiles) {
+            String inputFilePath = trueInputPath + f.getName();
+            WarcReader warcReader = parser.readWarc(inputFilePath);
+            System.out.println(inputFilePath);
+            //TO-DO
+            // pass the input file into Postings class to process
+        }
+    }
 }
